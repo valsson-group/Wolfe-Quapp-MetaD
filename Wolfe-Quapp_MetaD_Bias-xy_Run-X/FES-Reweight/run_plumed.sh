86990569c6efc381d175a3b73f390b5a2fceb72c
Omar Valsson <omar.valsson@gmail.com>
2025-07-17 14:27:29 -0500

    add files to remove initial part of colvar file

--- Wolfe-Quapp_MetaD_Bias-xy_Run-X/FES-Reweight/run_plumed.sh
+++ Wolfe-Quapp_MetaD_Bias-xy_Run-X/FES-Reweight/run_plumed.sh
@@ -1,1 +1,5 @@
+InitialPartToSkip=1000
+
+./trim-colvar-file.py --colvar-file ../colvar.data --output-file colvar.skip-inital-part.data --time-min ${InitialPartToSkip}
+
 plumed driver --noatoms --plumed plumed_FES-reweight.dat
